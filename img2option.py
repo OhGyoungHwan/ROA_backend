fca21b193940b1042e59489e2354badaab5abb9e
OhGyoungHwan <rudghks531@naver.com>
2022-12-04 21:37:01 +0900

    first commit

---
+++ img2option.py
@@ -0,0 +1,60 @@
+import pytesseract
+import cv2
+import matplotlib.pyplot as plt
+import imutils
+import re
+import requests
+import numpy as np
+from imutils.perspective import four_point_transform
+import pandas as pd
+import optioncompiler
+import json
+
+
+def imgfile_preprocessing(imgfile):
+    """
+    Input: bytes (image)
+    Output: numpy.ndarray (image)
+
+    읽혀진 이미지파일을 전처리 후 노이즈가 제거된 2진 이미지파일로 돌려준다.
+    """
+    image_nparray = np.asarray(bytearray(imgfile), dtype=np.uint8)
+    # binary이미지 파일을 numpy array로 변환
+    org_image = cv2.imdecode(image_nparray, cv2.IMREAD_COLOR)
+    # numpy array를 이미지로 디코딩
+    rgb_image = cv2.cvtColor(org_image, cv2.COLOR_BGR2XYZ)
+    # 이미지를 BGR에서 RGB로 변환
+    gray_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2GRAY)
+    # RGB이미지를 흑백이미지로 변환
+    binary_image = cv2.threshold(
+        gray_image, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    # 흑백이미지를 2진 이미지로 변환
+    removenoise_image = cv2.medianBlur(binary_image, ksize=1)
+    # 2진 이미지에서 노이즈를 제거
+    return removenoise_image
+
+
+def text_separateoption(text, type, rarity):
+    compile_RE = optioncompiler.DICT_OPTION_COMPILER[type+rarity]
+    option_compiler = re.compile(r'{0}'.format(compile_RE))
+    matchobj = option_compiler.findall(text)
+    option_list = [i.replace(' ', '') for i in matchobj]
+    option_figure_list = ["/".join(re.findall(r'\d+', i)) for i in option_list]
+    option_name_list = ["".join(re.findall(r'[^0-9+%(){}\-]+', i))
+                        for i in option_list]
+    return dict(zip(option_name_list, option_figure_list))
+
+
+def img2text(img, type, rarity):
+    """
+    Input: bytes (image), type (text: enum), rarity (text: enum)
+    Output: json
+
+    들어온 이미지를 분석해 아이템 옵션을 추출하고 json형태로 돌려준다.
+    """
+    processed_img = imgfile_preprocessing(img)
+
+    text = pytesseract.image_to_string(
+        processed_img, lang='kor', config="--psm 6")
+    text = text.replace("7|", "기")
+    return text_separateoption(text, type, rarity)
---
+++ main.py
@@ -0,0 +1,70 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from enum import Enum
+import img2option
+import datetime
+
+
+class Type(str, Enum):
+    주얼 = "주얼"
+    작은부적 = "작은부적"
+    거대부적 = "거대부적"
+    단도 = "단도"
+    도검 = "도검"
+    도끼 = "도끼"
+    곤봉 = "곤봉"
+    철퇴 = "철퇴"
+    망치 = "망치"
+    셉터 = "셉터"
+    지팡이 = "지팡이"
+    원드 = "원드"
+    창 = "창"
+    미늘창 = "미늘창"
+    손톱 = "손톱"
+    오브 = "오브"
+    아마존창 = "아마존창"
+    투척단도 = "투척단도"
+    투척도끼 = "투척도끼"
+    투창 = "투창"
+    활쇠뇌 = "활/쇠뇌"
+    아마존활 = "아마존활"
+    서클릿 = "서클릿"
+    투구 = "투구"
+    갑옷 = "갑옷"
+    장갑 = "장갑"
+    방패 = "방패"
+    허리띠 = "허리띠"
+    신발 = "신발"
+    바바투구 = "바바투구"
+    드루가죽 = "드루가죽"
+    네크토템 = "네크토템"
+    팔라방패 = "팔라방패"
+    반지 = "반지"
+    목걸이 = "목걸이"
+
+
+class Rarity(str, Enum):
+    매직 = "매직"
+    레어 = "레어"
+
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.post("/uploadimg/")
+async def create_upload_file(file: UploadFile, type: Type, rarity: Rarity):
+    images = await file.read()
+    option = img2option.img2text(images, type, rarity)
+    print(datetime.datetime.now(), option)
+    return option
